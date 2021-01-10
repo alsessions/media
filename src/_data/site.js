447bea99e6300689dc3bbdd54f9c73e8c95dbca8
alsessions <fultonchain@gmail.com>
2021-01-10 15:37:08 -0500

    First commit.

---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Media List",
+  description: "A quick look at what we're consuming and an eleventy playground.",
+  keywords: ['eleventy', 'blog', 'owl web dev', 'books','media'],
+  // your site url without trailing slash
+  // url: 'https://vredeburg.netlify.app',
+  // how many posts you want to show for each page
+  paginate: 6
+  // if you want to add disqus to your site
+  // disqusShortname: "your-shortname"
+};
